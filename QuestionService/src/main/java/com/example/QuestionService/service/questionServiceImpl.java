6dbafb3c1fa291a1f1b4389612c5d4236e37a532
Hriday jain <hridayguna@gmail.com>
2025-10-25 01:39:20 +0530

    commiting after making things configurable and running docker images locally

--- QuestionService/src/main/java/com/example/QuestionService/service/questionServiceImpl.java
+++ QuestionService/src/main/java/com/example/QuestionService/service/questionServiceImpl.java
@@ -10,6 +10,8 @@ import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -47,7 +49,7 @@ public class questionServiceImpl implements questionService {
 
     @Override
     public List<question> getQuestionsByQuizId(Long id) {
-        return  questionRepo.findByQuizId(id);
+        return questionRepo.findByQuizId(id);
     }
 
     @RateLimiter(name = "QuizBreaker",fallbackMethod = "QuizBreakerFallbackMethod")
